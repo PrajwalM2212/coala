5bb4176b52bae3ce4fd00e1047c9a6fc88879061
PrajwalM2212 <prajwalmmath@gmail.com>
2019-06-26 13:01:12 +0530

    writer2

--- coalib/output/TomlConfWriter.py
+++ coalib/output/TomlConfWriter.py
@@ -1,6 +1,6 @@
 from coalib.parsing.TomlConfParser import TomlSetting
-from tomlkit import document, table, dumps, array, string, boolean, integer, comment
-from tomlkit.items import Array, String, Bool, Integer, Comment
+from tomlkit import document, table, dumps, array, string, key, integer, comment
+from tomlkit.items import Array, String, Bool, Integer, Comment, Key, KeyType
 
 
 class TomlConfWriter:
@@ -13,31 +13,47 @@ class TomlConfWriter:
             section = self.sections[item]
             table_name = section.name
             table_contents = table()
-            for key, setting in section.contents.items():
-                key = setting.key
+            for _, setting in section.contents.items():
+
+                if ':' in setting.key:
+                    count = setting.key.count(':')
+                    setting_key = Key(setting.key.replace(':', '.', count),
+                                      t=KeyType.Bare,
+                                      dotted=True)
+                else:
+                    setting_key = key(setting.key)
 
                 if isinstance(setting, TomlSetting):
                     value = setting.original_value
                 else:
-                    value = setting.value
+                    value = self.get_value_type(setting.value)
 
                 if isinstance(value, Array):
-                    table_contents.add(key, array(value.as_string()))
-                    table_contents[key].comment(value.trivia.comment)
+                    table_contents.add(setting_key, array(value.as_string()))
+                    table_contents[setting_key].comment(value.trivia.comment)
                 elif isinstance(value, String):
-                    table_contents.add(key, string(value))
-                    table_contents[key].comment(value.trivia.comment)
+                    table_contents.add(setting_key, string(value))
+                    table_contents[setting_key].comment(value.trivia.comment)
                 elif isinstance(value, Bool):
-                    table_contents.add(key, boolean(value.as_string()))
-                    table_contents[key].comment(value.trivia.comment)
+                    table_contents.add(setting_key, value)
                 elif isinstance(value, Integer):
-                    table_contents.add(key, integer(value.as_string()))
-                    table_contents[key].comment(value.trivia.comment)
+                    table_contents.add(setting_key, integer(value.as_string()))
+                    table_contents[setting_key].comment(value.trivia.comment)
                 elif isinstance(value, Comment):
                     table_contents.add(comment(value.as_string()))
-                elif isinstance(value, str):
-                    table_contents.add(key, value)
+                elif isinstance(value, (str, bool, int)):
+                    table_contents.add(setting_key, value)
 
             self.document.add(table_name, table_contents)
 
         print(dumps(self.document))
+
+    def get_value_type(self, value):
+        try:
+            value = int(value)
+        except ValueError:
+            try:
+                value = bool(value)
+            except ValueError:
+                pass
+        return value
